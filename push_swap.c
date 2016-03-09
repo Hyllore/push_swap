9990caec1cf50d0e9f5624eaab5bc5c671c6c341
Roly <dorian97@free.fr>
2016-03-09 22:41:00 +0100

    travail a la maison

--- push_swap.c
+++ push_swap.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 void	error(void)
 {
@@ -18,18 +19,49 @@ void	error(void)
 	exit(0);
 }
 
+t_lst	push(t_lst *lst_1, t_lst *lst_2, t_lst *tmp)
+{
+	if (lst_1 == NULL)
+	{
+		lst_1 = malloc(sizeof(t_lst));
+		lst_1->next = NULL;
+	}
+	tmp = lst_1;
+	while (lst_2->next->next != NULL)
+		lst_2 = lst_2->next;
+	while (lst_1->next != NULL)
+		lst_1 = lst_1->next;
+	lst_1->content = lst_2->content;
+	lst_1 = tmp;
+	ft_putnbr(lst_1->next->content);
+	return (*lst_1);
+}
+
+t_lst	swap_ab(t_lst *lst_ab, t_lst *tmp)
+{
+	int c;
+
+	c = 0;
+	while (lst_ab->next->next->next != NULL)
+		lst_ab = lst_ab->next;
+	c = lst_ab->content;
+	lst_ab->content = lst_ab->next->content;
+	lst_ab->next->content = c;
+	lst_ab = tmp;
+	return (*lst_ab);
+}
+
 t_lst	put_in_lst(t_lst *lst_a, char **argv, int argc)
 {
 	int i;
 	t_lst *tmp;
+	t_lst *lst_b;
 
 	argc--;
 	tmp = lst_a;
 	i = 1;
 	while (argc > 0)
 	{
-		if (ft_isdigit(argv[i]) == 0)
-			error();
 		lst_a->next = malloc(sizeof(t_lst));
 		lst_a->content = ft_atoi(argv[i]);
 		if (ft_strcmp(ft_itoa(lst_a->content), argv[i]) != 0)
@@ -40,6 +72,14 @@ t_lst	put_in_lst(t_lst *lst_a, char **argv, int argc)
 	}
 	lst_a->next = NULL;
 	lst_a = tmp;
+	*lst_b = push(lst_b, lst_a, tmp);
+	ft_putchar('c');
+	while (lst_b->next != NULL)
+	{
+		ft_putchar('c');
+		printf("lst b : %d", lst_b->content);
+		lst_b = lst_b->next;
+	}
 	return (*lst_a);
 }
 
