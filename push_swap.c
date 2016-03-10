f6e0cdd64e5015d05cbe4e5947e9bf6e491d06ba
Dorian ROLY <droly@e1r6p12.42.fr>
2016-03-10 16:58:48 +0100

    jarivpa

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: droly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 14:09:00 by droly             #+#    #+#             */
-/*   Updated: 2016/03/07 17:48:41 by droly            ###   ########.fr       */
+/*   Updated: 2016/03/10 16:58:34 by droly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,38 @@ void	error(void)
 	exit(0);
 }
 
-t_lst	push(t_lst *lst_1, t_lst *lst_2, t_lst *tmp)
+t_lst	*push2(t_lst *lst_2, t_lst *tmp)
 {
+	tmp = lst_2;
+	while (lst_2->next != NULL)
+		lst_2 = lst_2->next;
+	free(lst_2);
+	lst_2 = NULL;
+	lst_2 = tmp;
+	return (lst_2);
+}
+
+t_lst	*push(t_lst *lst_1, t_lst *lst_2, t_lst *tmp)
+{
+	while (lst_2->next != NULL)
+		lst_2 = lst_2->next;
 	if (lst_1 == NULL)
 	{
 		lst_1 = malloc(sizeof(t_lst));
 		lst_1->next = NULL;
+		tmp = lst_1;
+		lst_1->content = lst_2->content;
+		lst_1 = tmp;
+		return (lst_1);
 	}
 	tmp = lst_1;
-	while (lst_2->next->next != NULL)
-		lst_2 = lst_2->next;
-	while (lst_1->next != NULL)
+	while (lst_1 != NULL)
 		lst_1 = lst_1->next;
+	lst_1 = malloc(sizeof(t_lst));
 	lst_1->content = lst_2->content;
+	lst_1->next = NULL;
 	lst_1 = tmp;
-	ft_putnbr(lst_1->next->content);
-	return (*lst_1);
+	return (lst_1);
 }
 
 t_lst	swap_ab(t_lst *lst_ab, t_lst *tmp)
@@ -42,7 +58,7 @@ t_lst	swap_ab(t_lst *lst_ab, t_lst *tmp)
 	int c;
 
 	c = 0;
-	while (lst_ab->next->next->next != NULL)
+	while (lst_ab->next != NULL)
 		lst_ab = lst_ab->next;
 	c = lst_ab->content;
 	lst_ab->content = lst_ab->next->content;
@@ -51,18 +67,23 @@ t_lst	swap_ab(t_lst *lst_ab, t_lst *tmp)
 	return (*lst_ab);
 }
 
-t_lst	put_in_lst(t_lst *lst_a, char **argv, int argc)
+t_lst	*put_in_lst(t_lst *lst_a, char **argv, int argc)
 {
 	int i;
 	t_lst *tmp;
 	t_lst *lst_b;
+	int i2;
 
+	lst_b = NULL;
 	argc--;
+	i2 = argc;
 	tmp = lst_a;
 	i = 1;
 	while (argc > 0)
 	{
-		lst_a->next = malloc(sizeof(t_lst));
+		ft_putchar('i');
+		if (i2 != argc)
+			lst_a = malloc(sizeof(t_lst));
 		lst_a->content = ft_atoi(argv[i]);
 		if (ft_strcmp(ft_itoa(lst_a->content), argv[i]) != 0)
 			error();
@@ -70,29 +91,45 @@ t_lst	put_in_lst(t_lst *lst_a, char **argv, int argc)
 		argc--;
 		lst_a = lst_a->next;
 	}
-	lst_a->next = NULL;
+	lst_a = NULL;
 	lst_a = tmp;
-	*lst_b = push(lst_b, lst_a, tmp);
-	ft_putchar('c');
-	while (lst_b->next != NULL)
+	while (lst_a->next != NULL)
 	{
 		ft_putchar('c');
-		printf("lst b : %d", lst_b->content);
-		lst_b = lst_b->next;
+		printf("\nlst a-1 : %d\n", lst_a->content);
+		lst_a = lst_a->next;
 	}
-	return (*lst_a);
+	lst_b = push(lst_b, lst_a, tmp);
+	lst_a = push2(lst_a, tmp);
+	while (lst_a != NULL)
+	{
+		tmp = lst_a;
+		printf("\nlst a : %d\n", lst_a->content);
+		lst_a = lst_a->next;
+	}
+	lst_a = tmp;
+	lst_b = push(lst_b, lst_a, tmp);
+	lst_a = push2(lst_a, tmp);
+	while (lst_a != NULL)
+	{
+		tmp = lst_a;
+		printf("\nlst a2 : %d\n", lst_a->content);
+		lst_a = lst_a->next;
+	}
+	lst_a = tmp;
+	return (lst_a);
 }
 
 int		main(int argc, char **argv)
 {
 	t_lst *lst_a;
 
-	lst_a = (t_lst*)malloc(sizeof(t_lst));
-	*lst_a = put_in_lst(lst_a, argv, argc);
-	while (lst_a->next != NULL)
-	{
-		ft_putnbr(lst_a->content);
-		lst_a = lst_a->next;
-	}
-	lst_a = lst_a->next;
+	lst_a = malloc(sizeof(t_lst));
+	lst_a = put_in_lst(lst_a, argv, argc);
+//	while (lst_a->next != NULL)
+//	{
+//		printf("\nlst a : %d\n", lst_a->content);
+//		lst_a = lst_a->next;
+//	}
+//	lst_a = lst_a->next;
 }
