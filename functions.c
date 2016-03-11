0fcf4a0b42f77a53a4534b024a73ac9da13f2f7b
Dorian ROLY <droly@e1r6p12.42.fr>
2016-03-11 18:00:23 +0100

    creation functions.c

---
+++ functions.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: droly <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/11 17:22:51 by droly             #+#    #+#             */
+/*   Updated: 2016/03/11 17:48:09 by droly            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_lst	*push2(t_lst *lst_2, t_lst *tmp)
+{
+	tmp = lst_2;
+	while (lst_2->next->next != NULL)
+		lst_2 = lst_2->next;
+	lst_2->next = NULL;
+	lst_2 = tmp;
+	return (lst_2);
+}
+
+t_lst	*rotate_r(t_lst *lst_ab, t_lst *tmp)
+{
+	int tmp2;
+
+	tmp2 = lst_ab->content;
+	tmp = lst_ab;
+	while (lst_ab->next != NULL)
+	{
+		lst_ab->content = lst_ab->next->content;
+		lst_ab = lst_ab->next;
+	}
+	lst_ab->content = tmp2;
+	lst_ab = tmp;
+	return (lst_ab);
+}
+
+t_lst	*rotate(t_lst *lst_ab, t_lst *tmp)
+{
+	int tmp2;
+	int tmp3;
+
+	tmp3 = 0;
+	tmp2 = 0;
+	tmp = lst_ab;
+	while (lst_ab->next != NULL)
+		lst_ab = lst_ab->next;
+	tmp2 = lst_ab->content;
+	lst_ab = tmp;
+	while (lst_ab != NULL)
+	{
+		tmp3 = lst_ab->content;
+		lst_ab->content = tmp2;
+		tmp2 = tmp3;
+		lst_ab = lst_ab->next;
+	}
+	lst_ab = tmp;
+	return (lst_ab);
+}
+
+t_lst	*push(t_lst *lst_1, t_lst *lst_2, t_lst *tmp)
+{
+	while (lst_2->next != NULL)
+		lst_2 = lst_2->next;
+	if (lst_1 == NULL)
+	{
+		lst_1 = malloc(sizeof(t_lst));
+		lst_1->next = NULL;
+		tmp = lst_1;
+		lst_1->content = lst_2->content;
+		lst_1 = tmp;
+		return (lst_1);
+	}
+	tmp = lst_1;
+	while (lst_1->next != NULL)
+		lst_1 = lst_1->next;
+	lst_1->next = malloc(sizeof(t_lst));
+	lst_1->next->next = NULL;
+	lst_1->next->content = lst_2->content;
+	lst_1 = tmp;
+	return (lst_1);
+}
+
+t_lst	*swap_ab(t_lst *lst_ab, t_lst *tmp)
+{
+	int c;
+
+	c = 0;
+	tmp = lst_ab;
+	while (lst_ab->next->next != NULL)
+		lst_ab = lst_ab->next;
+	c = lst_ab->content;
+	lst_ab->content = lst_ab->next->content;
+	lst_ab->next->content = c;
+	lst_ab = tmp;
+	return (lst_ab);
+}
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: droly <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 14:09:00 by droly             #+#    #+#             */
-/*   Updated: 2016/03/10 16:58:34 by droly            ###   ########.fr       */
+/*   Updated: 2016/03/11 17:59:48 by droly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,52 +19,23 @@ void	error(void)
 	exit(0);
 }
 
-t_lst	*push2(t_lst *lst_2, t_lst *tmp)
+void	checkdoublon(t_lst *lst_a, t_lst *tmp)
 {
-	tmp = lst_2;
-	while (lst_2->next != NULL)
-		lst_2 = lst_2->next;
-	free(lst_2);
-	lst_2 = NULL;
-	lst_2 = tmp;
-	return (lst_2);
-}
+	int tmp2;
 
-t_lst	*push(t_lst *lst_1, t_lst *lst_2, t_lst *tmp)
-{
-	while (lst_2->next != NULL)
-		lst_2 = lst_2->next;
-	if (lst_1 == NULL)
+	tmp2 = 0;
+	while (lst_a != NULL)
 	{
-		lst_1 = malloc(sizeof(t_lst));
-		lst_1->next = NULL;
-		tmp = lst_1;
-		lst_1->content = lst_2->content;
-		lst_1 = tmp;
-		return (lst_1);
+		tmp2 = lst_a->content;
+		tmp = lst_a->next;
+		while (tmp != NULL)
+		{
+			if (tmp2 == tmp->content)
+				error();
+			tmp = tmp->next;
+		}
+		lst_a = lst_a->next;
 	}
-	tmp = lst_1;
-	while (lst_1 != NULL)
-		lst_1 = lst_1->next;
-	lst_1 = malloc(sizeof(t_lst));
-	lst_1->content = lst_2->content;
-	lst_1->next = NULL;
-	lst_1 = tmp;
-	return (lst_1);
-}
-
-t_lst	swap_ab(t_lst *lst_ab, t_lst *tmp)
-{
-	int c;
-
-	c = 0;
-	while (lst_ab->next != NULL)
-		lst_ab = lst_ab->next;
-	c = lst_ab->content;
-	lst_ab->content = lst_ab->next->content;
-	lst_ab->next->content = c;
-	lst_ab = tmp;
-	return (*lst_ab);
 }
 
 t_lst	*put_in_lst(t_lst *lst_a, char **argv, int argc)
@@ -78,44 +49,28 @@ t_lst	*put_in_lst(t_lst *lst_a, char **argv, int argc)
 	argc--;
 	i2 = argc;
 	tmp = lst_a;
-	i = 1;
+	i = argc;
 	while (argc > 0)
 	{
-		ft_putchar('i');
-		if (i2 != argc)
-			lst_a = malloc(sizeof(t_lst));
+		if (argc != 1)
+			lst_a->next = (t_lst*)malloc(sizeof(t_lst));
 		lst_a->content = ft_atoi(argv[i]);
 		if (ft_strcmp(ft_itoa(lst_a->content), argv[i]) != 0)
 			error();
-		i++;
+		i--;
 		argc--;
-		lst_a = lst_a->next;
+		if (argc != 0)
+			lst_a = lst_a->next;
 	}
-	lst_a = NULL;
+	lst_a->next = NULL;
 	lst_a = tmp;
-	while (lst_a->next != NULL)
-	{
-		ft_putchar('c');
-		printf("\nlst a-1 : %d\n", lst_a->content);
-		lst_a = lst_a->next;
-	}
-	lst_b = push(lst_b, lst_a, tmp);
-	lst_a = push2(lst_a, tmp);
-	while (lst_a != NULL)
-	{
-		tmp = lst_a;
-		printf("\nlst a : %d\n", lst_a->content);
-		lst_a = lst_a->next;
-	}
-	lst_a = tmp;
-	lst_b = push(lst_b, lst_a, tmp);
-	lst_a = push2(lst_a, tmp);
-	while (lst_a != NULL)
-	{
-		tmp = lst_a;
-		printf("\nlst a2 : %d\n", lst_a->content);
-		lst_a = lst_a->next;
-	}
+	checkdoublon(lst_a, tmp);
+	lst_a = rotate_r(lst_a, tmp);
+	lst_a = rotate_r(lst_a, tmp);
+	lst_a = rotate_r(lst_a, tmp);
+	lst_a = rotate_r(lst_a, tmp);
+	lst_a = rotate_r(lst_a, tmp);
+	lst_a = rotate_r(lst_a, tmp);
 	lst_a = tmp;
 	return (lst_a);
 }
@@ -124,12 +79,13 @@ int		main(int argc, char **argv)
 {
 	t_lst *lst_a;
 
-	lst_a = malloc(sizeof(t_lst));
+	lst_a = (t_lst*)malloc(sizeof(t_lst));
+	lst_a->next = NULL;
 	lst_a = put_in_lst(lst_a, argv, argc);
-//	while (lst_a->next != NULL)
-//	{
-//		printf("\nlst a : %d\n", lst_a->content);
-//		lst_a = lst_a->next;
-//	}
-//	lst_a = lst_a->next;
+	ft_putstr("lst a :");
+	while (lst_a != NULL)
+	{
+		printf(" %d", lst_a->content);
+		lst_a = lst_a->next;
+	}
 }
